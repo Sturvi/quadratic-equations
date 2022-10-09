713e6068ebaa1510d1c823291a193367dd73bb3f
Emin <114976816+Sturvi@users.noreply.github.com>
2022-10-10 00:31:23 +0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,17 @@
-import java.util.Arrays;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        double[] variables = VvodDannix();
-        double d = D(variables);
+        double[] variables = imputData();
+        double d = discriminant(variables);
         if (variables[0] != 0) {
             if (d > 0) {
-                DBolshe0(variables, d);
+                ifDiscriminantGreaterThan0(variables, d);
             }
             if (d == 0) {
-                DRavno0(variables);
+                ifDiscriminantEqualTo0(variables);
             }
             if (d < 0)
                 System.out.println("Действительных корней нет");
@@ -24,7 +23,7 @@ public class Main {
     }
 
     /*Считывание данных с клавиатуры и добавление всех коифицентов уравнения в однельный массив.*/
-    public static double[] VvodDannix() {
+    public static double[] imputData() {
         Scanner scan = new Scanner(System.in);
         boolean test=false;
         String equation="";
@@ -40,18 +39,18 @@ public class Main {
         }
         String temp = ("+" + equation).replaceAll("\\s", "");
         /*Следующие 4 строки добавляют цифру 1 перед переменными без множителя*/
-        temp = temp.replaceAll("\\+x\\^2", "\\+1x\\^2");
-        temp = temp.replaceAll("\\-x\\^2", "\\-1x\\^2");
-        temp = temp.replaceAll("\\+x[^\\^]", "\\+1x");
-        temp = temp.replaceAll("\\-x[^\\^]", "\\-1x");
+        temp = temp.replaceAll("\\+x", "\\+1x");
+        temp = temp.replaceAll("\\-x", "\\-1x");
+        /*temp = temp.replaceAll("\\+x{0}[^\\^]", "\\+1x");
+        temp = temp.replaceAll("\\-x{0}[^\\^]", "\\-1x");*/
         double[] variables = new double[3];
-        variables[0]=Variables(temp,"([+-][0-9]*)x\\^2");   //a
-        variables[1]=Variables(temp,"([+-][0-9]*)x(?!\\^)");    //b
-        variables[2]=Variables(temp, "([+-][0-9]+)(?!x)");  //c
+        variables[0]= determineTheCoefficient(temp,"([+-][0-9]*)x\\^2");   //a
+        variables[1]= determineTheCoefficient(temp,"([+-][0-9]*)x(?!\\^)");    //b
+        variables[2]= determineTheCoefficient(temp, "([+-][0-9]+)(?!x)");  //c
         return  variables;
     }
 
-    public static double Variables (String equation, String regex) {
+    public static double determineTheCoefficient(String equation, String regex) {
         Pattern patt = Pattern.compile(regex);
         Matcher match = patt.matcher(equation);
         double variables = 0;
@@ -65,19 +64,19 @@ public class Main {
     }
 
     /*Вычисляем дискриминант уравнения*/
-    public static double D(double[] intvariables) {
+    public static double discriminant(double[] intvariables) {
         Double d = (intvariables[1] * intvariables[1]) - (4.0 * intvariables[0] * intvariables[2]);
         return (d);
     }
 
    /* Вычисление уравнения в случае когда Дискриминант равен 0*/
-    public static void  DRavno0(double[] variables) {
+    public static void ifDiscriminantEqualTo0(double[] variables) {
         double x = ((-1.0) * variables[1]) / (2.0 * variables[0]);
         System.out.println("X=" + x);
             }
 
     /* Вычисление уравнения в случае когда Дискриминант больше 0*/
-    public static void  DBolshe0(double[] intvariables, double d) {
+    public static void ifDiscriminantGreaterThan0(double[] intvariables, double d) {
         double[] x = new double[2];
         x[0] = (-intvariables[1] + Math.sqrt(d)) / (2 * intvariables[0]);
         x[1] = (-intvariables[1] - Math.sqrt(d)) / (2 * intvariables[0]);
